e97ecfffefe8ccc25e7044c2bd8354797f358b5f
renee.llup <renee.llup@gmail.com>
2020-07-11 15:28:42 +0800

    94. Controlling the useEffect() Behavior

--- react-complete-guide/src/components/Cockpit/Cockpit.js
+++ react-complete-guide/src/components/Cockpit/Cockpit.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import classes from "./Cockpit.css";
 
 const cockpit = (props) => {
+  useEffect(() => {
+    console.log("[Cockpit.js] useEffect");
+    // Http request here....
+    setTimeout(() => {
+      alert("Save data to cloud!");
+    }, 1000);
+  }, []);
+
   const assignedClasses = [];
   let btnClass = "";
 
