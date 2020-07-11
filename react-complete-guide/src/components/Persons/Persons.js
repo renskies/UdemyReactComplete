728632ae97bab64ad344e8451a1fa49a4b865dc2
renee.llup <renee.llup@gmail.com>
2020-07-11 12:32:23 +0800

    91. Component Update Lifecycle (for props Changes)

--- react-complete-guide/src/components/Persons/Persons.js
+++ react-complete-guide/src/components/Persons/Persons.js
@@ -1,21 +1,51 @@
-import React from "react";
+import React, { Component } from "react";
 import Person from "../Persons/Person/Person";
 import ErrorBoundary from "../../ErrorBoundary/ErrorBoundary";
 
-const persons = (props) => {
-  console.log("[Persons.js] rendering...");
-  return props.persons.map((person, index) => {
-    return (
-      <ErrorBoundary key={person.id}>
-        <Person
-          click={() => props.clicked(index)}
-          name={person.name}
-          age={person.age}
-          changed={(event) => props.changed(event, person.id)}
-        />
-      </ErrorBoundary>
-    );
-  });
-};
+class Persons extends Component {
+  // static getDerivedStateFromProps(props, state) {
+  //   console.log("[Persons.js] getDerivedStateFromProps");
+  //   return state;
+  // }
 
-export default persons;
+  // componentWillReceiveProps(nextProps) {
+  //   console.log("[Persons.js] componentWillReceiveProps", nextProps);
+  // }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("[Persons.js] shouldComponentUpdate");
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log("[Persons.js] getSnapshotBeforeUpdate");
+    return { message: "Snapshot!" };
+  }
+
+  // componentWillUpdate() {
+  //   console.log("[Persons.js] componentWillUpdate");
+  // }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log("[Persons.js] componentDidUpdate");
+    console.log(snapshot);
+  }
+
+  render() {
+    console.log("[Persons.js] rendering...");
+    return this.props.persons.map((person, index) => {
+      return (
+        <ErrorBoundary key={person.id}>
+          <Person
+            click={() => this.props.clicked(index)}
+            name={person.name}
+            age={person.age}
+            changed={(event) => this.props.changed(event, person.id)}
+          />
+        </ErrorBoundary>
+      );
+    });
+  }
+}
+
+export default Persons;
